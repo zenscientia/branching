0cbab3c58347c101db04f2995cab95305878e77f
zenscientia <zenscientia@gmail.com>
2013-08-15 20:54:48 -0700

    return content throuh index.html

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var http = require("http");
 http.createServer(function (req, res) {
 
 	res.writeHead(200, { "Content-Type": "text/plain" });
-	
+	res.write("This is a demo app");
+	res.end();
 
 }).listen(3000);
 
