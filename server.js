9c8d0aa771aff906d9ef9b7ac09df96ac1c4ab71
zenscientia <zenscientia@gmail.com>
2013-08-15 20:47:26 -0700

    added correct response headers

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var http = require("http");
 
 http.createServer(function (req, res) {
 
+	res.writeHead(200, { "Content-Type": "text/plain" });
+	
+
 }).listen(3000);
 
 console.log("listening at port 3000...");
